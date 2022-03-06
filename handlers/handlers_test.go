860152faee563d2bfe13fe37d52200fc58391f90
shevvvo <vasily.shevchenko2222222@gmail.com>
2022-03-06 16:04:04 +0300

    PLANEXA-1: попытаться написать нормальный тест

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,17 +1,57 @@
 package handlers
 
 import (
+	"PLANEXA_backend/models"
+	"PLANEXA_backend/routes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"sync"
 	"testing"
 )
 
 var (
-	getBoardUrl = "/api/"
+	router = gin.New()
+	lock   = sync.RWMutex{}
 )
 
-func testGetBoards(t *testing.T) {
+func TestGetBoards(t *testing.T) {
 	t.Parallel()
-	request, _ := http.NewRequest("GET", getBoardUrl, nil)
-	cookie := &http.Cookie{Name: ""}
-	request.AddCookie()
+
+	request, _ := http.NewRequest("GET", routes.HomeRoute, nil)
+	cookie := &http.Cookie{
+		Name:  "1",
+		Value: "session1",
+	}
+	request.AddCookie(cookie)
+	writer := httptest.NewRecorder()
+	router.ServeHTTP(writer, request)
+
+	if writer.Code != http.StatusOK {
+		t.Error("status is not ok")
+	}
+
+	var returnedBoards []models.Board
+	err := json.Unmarshal(writer.Body.Bytes(), &returnedBoards)
+	if err != nil {
+		t.Error(err)
+	}
+
+	lock.RLock()
+	expectedBoards := models.BoardList
+	isEqual := true
+
+	if len(expectedBoards) != len(returnedBoards) {
+		isEqual = false
+	}
+
+	if !reflect.DeepEqual(returnedBoards, expectedBoards) {
+		isEqual = false
+	}
+	lock.RUnlock()
+
+	require.True(t, isEqual)
 }
