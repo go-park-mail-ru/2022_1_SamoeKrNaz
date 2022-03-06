a250931fd76969635f771ab775ecde8c903ccffd
shevvvo <vasily.shevchenko2222222@gmail.com>
2022-03-06 16:49:09 +0300

    PLANEXA-1: добавить тесты

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"PLANEXA_backend/models"
 	"PLANEXA_backend/routes"
+	"bytes"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"reflect"
 	"sync"
 	"testing"
@@ -18,12 +20,22 @@ var (
 	lock   = sync.RWMutex{}
 )
 
-func TestGetBoards(t *testing.T) {
+func TestMain(m *testing.M) {
+	mainRoutes := router.Group(routes.HomeRoute)
+	{
+		mainRoutes.POST(routes.LoginRoute, Login)
+		mainRoutes.POST(routes.RegisterRoute, Register)
+		mainRoutes.GET("", GetBoards)
+	}
+	os.Exit(m.Run())
+}
+
+func TestGetBoardsSuccess(t *testing.T) {
 	t.Parallel()
 
 	request, _ := http.NewRequest("GET", routes.HomeRoute, nil)
 	cookie := &http.Cookie{
-		Name:  "1",
+		Name:  "token",
 		Value: "session1",
 	}
 	request.AddCookie(cookie)
@@ -54,4 +66,46 @@ func TestGetBoards(t *testing.T) {
 	lock.RUnlock()
 
 	require.True(t, isEqual)
+}
+
+func TestGetBoardsFalse(t *testing.T) {
+	t.Parallel()
+
+	request, _ := http.NewRequest("GET", routes.HomeRoute, nil)
+	cookie := &http.Cookie{
+		Name:  "token",
+		Value: "sessionFalse",
+	}
+	request.AddCookie(cookie)
+	writer := httptest.NewRecorder()
+	router.ServeHTTP(writer, request)
+	require.Equal(t, writer.Code, http.StatusUnauthorized)
+}
+
+func TestLoginSuccess(t *testing.T) {
+	t.Parallel()
+
+	newUser := models.User{Username: "user1", Password: "pass1"}
+	jsonNewUser, _ := json.Marshal(newUser)
+	body := bytes.NewReader(jsonNewUser)
+
+	request, _ := http.NewRequest("POST", routes.HomeRoute+routes.LoginRoute, body)
+	writer := httptest.NewRecorder()
+	router.ServeHTTP(writer, request)
+
+	require.Equal(t, http.StatusOK, writer.Code)
+}
+
+func TestLoginFail(t *testing.T) {
+	t.Parallel()
+
+	newUser := models.User{Username: "user123", Password: "pass123"}
+	jsonNewUser, _ := json.Marshal(newUser)
+	body := bytes.NewReader(jsonNewUser)
+
+	request, _ := http.NewRequest("POST", routes.HomeRoute+routes.LoginRoute, body)
+	writer := httptest.NewRecorder()
+	router.ServeHTTP(writer, request)
+
+	require.Equal(t, http.StatusUnauthorized, writer.Code)
 }
