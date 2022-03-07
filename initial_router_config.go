5038217c0c37b3fcacb29e36500f8108b13793df
shevvvo <vasily.shevchenko2222222@gmail.com>
2022-03-07 20:24:45 +0300

    PLANEXA-1: тест для фронта5

--- initial_router_config.go
+++ initial_router_config.go
@@ -3,6 +3,7 @@ package main
 import (
 	"PLANEXA_backend/handlers"
 	"PLANEXA_backend/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,11 +17,12 @@ func initRouter() *gin.Engine {
 		mainRoutes.POST(routes.RegisterRoute, handlers.Register)
 	}
 
-	/*config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://planexa.netlify.app", "https://89.208.199.114:3000/", "https://localhost:8000"}
+	config := cors.DefaultConfig()
+	//config.AllowOrigins = []string{"https://planexa.netlify.app", "https://89.208.199.114:3000"}
+	config.AllowAllOrigins = true
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowCredentials = true
-	*/
-	//	router.Use(cors.New(config))
+
+	router.Use(cors.New(config))
 	return router
 }
