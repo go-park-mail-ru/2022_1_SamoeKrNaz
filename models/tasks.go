67f7f9946e33995b0317754946a775a5be192759
shevvvo <vasily.shevchenko2222222@gmail.com>
2022-03-08 23:09:22 +0300

    PLANEXA-1: добавить таски

---
+++ models/tasks.go
@@ -0,0 +1,14 @@
+package models
+
+type Task struct {
+	Id    uint   `json:"id"`
+	Title string `json:"title"`
+	Date  string `json:"date"`
+}
+
+var TaskList = []Task{
+	{1, "РК № 1. СУБД", "4 марта 2022 года"},
+	{2, "РК № 1. Фронт", "9 марта 2022 года"},
+	{3, "РК № 1. Go", "10 марта 2022 года"},
+	{4, "Спокойно выдохнуть", "После всех РК"},
+}
