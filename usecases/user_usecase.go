31e2ec2926553f0e20bca4a625ef303e5472fd11
shevvvo <vasily.shevchenko2222222@gmail.com>
2022-04-10 15:15:02 +0300

    a

--- usecases/user_usecase.go
+++ usecases/user_usecase.go
@@ -8,5 +8,5 @@ type UserUseCase interface {
 	Login(user models.User) (uint, string, error)
 	Register(user models.User) (uint, string, error)
 	Logout(token string) error
-	GetInfo(userId uint) (models.User, error)
+	GetInfoById(userId uint) (models.User, error)
 }
