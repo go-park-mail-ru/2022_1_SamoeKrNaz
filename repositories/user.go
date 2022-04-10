1a0d7535b1411f32b6d9ad64a45836db656d8007
yalublukotikov <yalublukotikov@yandex.ru>
2022-04-10 18:34:08 +0300

    Поправка докера

--- repositories/user.go
+++ repositories/user.go
@@ -1,7 +1,7 @@
 package repositories
 
 import (
-	customErrors "PLANEXA_backend/errors"
+	"PLANEXA_backend/errors"
 	"PLANEXA_backend/hash"
 	"PLANEXA_backend/models"
 	"github.com/kolesa-team/go-webp/decoder"
