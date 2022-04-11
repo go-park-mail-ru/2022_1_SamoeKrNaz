f7c5860bdcb5bb057b9c3130938474e8dbc238c0
shevvvo <vasily.shevchenko2222222@gmail.com>
2022-04-11 23:01:49 +0300

    that's good x2

--- cmd/api/initial_router_config.go
+++ cmd/api/initial_router_config.go
@@ -92,7 +92,7 @@ func initRouter() (*gin.Engine, error) {
 		mainRoutes.GET(routes.ProfileRoute+"/:id", middleware.CheckAuth, userHandler.GetInfoById)
 		mainRoutes.GET(routes.ProfileRoute, middleware.CheckAuth, userHandler.GetInfoByCookie)
 		mainRoutes.PUT(routes.ProfileRoute+"/upload", middleware.CheckAuth, userHandler.SaveAvatar)
-		mainRoutes.PUT(routes.ProfileRoute, middleware.CheckAuth)
+		mainRoutes.PUT(routes.ProfileRoute, middleware.CheckAuth, userHandler.RefactorProfile)
 	}
 	return router, nil
 }
