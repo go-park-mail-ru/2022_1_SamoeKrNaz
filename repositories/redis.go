5ee0f35e0e0f2e0e62dff4b6c1b9932babf68c90
yalublukotikov <yalublukotikov@yandex.ru>
2022-04-29 10:29:34 +0300

    новые правки

--- repositories/redis.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-import "PLANEXA_backend/models"
-
-type RedisRepository interface {
-	SetSession(session models.Session) error
-	GetSession(cookieValue string) (uint64, error)
-	DeleteSession(cookieValue string) error
-}
