1467a6bef83bb7de1b8b8967a7ae32849707238f
yalublukotikov <yalublukotikov@yandex.ru>
2022-05-04 11:11:38 +0300

    забыл

--- repositories/impl/user.go
+++ repositories/impl/user.go
@@ -97,7 +97,7 @@ func (userRepository *UserRepositoryImpl) SaveAvatar(user *models.User, header *
 
 func (userRepository *UserRepositoryImpl) IsAbleToLogin(username string, password string) (bool, error) {
 	isAble, err := userRepository.client.IsAbleToLogin(userRepository.ctx, &handler.CheckLog{Pass: password, Uname: &handler.Username{USERNAME: username}})
-	if strings.Contains(err.Error(), customErrors.ErrUserNotFound.Error()) {
+	if strings.Contains(err.Error(), customErrors.ErrUsernameNotExist.Error()) {
 		err = customErrors.ErrUserNotFound
 	}
 
