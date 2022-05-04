f252d795d42f49ffd5e9646ca324b652d69525e4
yalublukotikov <yalublukotikov@yandex.ru>
2022-05-04 12:03:33 +0300

    фикс метрик

--- user_microservice/server_user/handler/impl/handler.go
+++ user_microservice/server_user/handler/impl/handler.go
@@ -30,7 +30,7 @@ func (userServ *UserServerImpl) Create(ctx context.Context, in *handler.User) (*
 	userId, err := userServ.userUseCase.Create(&models.User{Username: in.UserData.Uname.USERNAME,
 		Password: in.UserData.Pass, IdU: uint(in.IDU.IDU), ImgAvatar: in.IMG})
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in create user").Inc()
+		metrics.User.WithLabelValues("500", "error in create user").Inc()
 		return &handler.IdUser{}, err
 	}
 	metrics.User.WithLabelValues("200", "success in create user").Inc()
@@ -47,7 +47,7 @@ func (userServ *UserServerImpl) Update(ctx context.Context, in *handler.User) (*
 	err := userServ.userUseCase.Update(&models.User{Username: in.UserData.Uname.USERNAME,
 		Password: in.UserData.Pass, IdU: uint(in.IDU.IDU), ImgAvatar: in.IMG})
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in update user").Inc()
+		metrics.User.WithLabelValues("500", "error in update user").Inc()
 		return &handler.NothingSec{}, err
 	}
 	metrics.User.WithLabelValues("200", "success in update user").Inc()
@@ -63,10 +63,10 @@ func (userServ *UserServerImpl) IsAbleToLogin(ctx context.Context, in *handler.C
 	}
 	is, err := userServ.userUseCase.IsAbleToLogin(in.Pass, in.Uname.USERNAME)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in isabletologin user").Inc()
+		metrics.User.WithLabelValues("500", "error in isabletologin user").Inc()
 		return &handler.NothingSec{}, err
 	}
-	metrics.Session.WithLabelValues("200", "success in isabletologin user").Inc()
+	metrics.User.WithLabelValues("200", "success in isabletologin user").Inc()
 	timer.ObserveDuration()
 	return &handler.NothingSec{Dummy: is}, nil
 }
@@ -79,10 +79,10 @@ func (userServ *UserServerImpl) AddUserToBoard(ctx context.Context, in *handler.
 	}
 	err := userServ.userUseCase.AddUserToBoard(uint(in.IDU.IDU), uint(in.IDB.IDB))
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in addusertoboard user").Inc()
+		metrics.User.WithLabelValues("500", "error in addusertoboard user").Inc()
 		return &handler.NothingSec{}, err
 	}
-	metrics.Session.WithLabelValues("200", "success in addusertoboard user").Inc()
+	metrics.User.WithLabelValues("200", "success in addusertoboard user").Inc()
 	timer.ObserveDuration()
 	return &handler.NothingSec{}, nil
 }
@@ -97,7 +97,7 @@ func (userServ *UserServerImpl) GetUserByLogin(ctx context.Context, in *handler.
 	user, err := userServ.userUseCase.GetUserByLogin(in.USERNAME)
 	fmt.Println("getuserbylogin", in.USERNAME, err)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in getuserbylogin user").Inc()
+		metrics.User.WithLabelValues("500", "error in getuserbylogin user").Inc()
 		return &handler.User{}, err
 	}
 	boardsBytes, err := json.Marshal(user.Boards)
@@ -105,7 +105,7 @@ func (userServ *UserServerImpl) GetUserByLogin(ctx context.Context, in *handler.
 		metrics.User.WithLabelValues("500", "error unmarshal in getuserbylogin user").Inc()
 		return &handler.User{}, err
 	}
-	metrics.Session.WithLabelValues("200", "success in getuserbylogin user").Inc()
+	metrics.User.WithLabelValues("200", "success in getuserbylogin user").Inc()
 	timer.ObserveDuration()
 	return &handler.User{IDU: &handler.IdUser{IDU: uint64(user.IdU)},
 		UserData: &handler.CheckLog{Uname: &handler.Username{USERNAME: user.Username}, Pass: user.Password},
@@ -121,15 +121,15 @@ func (userServ *UserServerImpl) GetUserById(ctx context.Context, in *handler.IdU
 	user, err := userServ.userUseCase.GetUserById(uint(in.IDU))
 	fmt.Println("getuserbyid", in, err)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in getuserbyid user").Inc()
+		metrics.User.WithLabelValues("500", "error in getuserbyid user").Inc()
 		return &handler.User{}, err
 	}
 	boardsBytes, err := json.Marshal(user.Boards)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error unmarshal in getuserbyid user").Inc()
+		metrics.User.WithLabelValues("500", "error unmarshal in getuserbyid user").Inc()
 		return &handler.User{}, err
 	}
-	metrics.Session.WithLabelValues("200", "success in getuserbyid user").Inc()
+	metrics.User.WithLabelValues("200", "success in getuserbyid user").Inc()
 	timer.ObserveDuration()
 	return &handler.User{IDU: &handler.IdUser{IDU: uint64(user.IdU)},
 		UserData: &handler.CheckLog{Uname: &handler.Username{USERNAME: user.Username}, Pass: user.Password},
@@ -145,10 +145,10 @@ func (userServ *UserServerImpl) IsExist(ctx context.Context, in *handler.Usernam
 	is, err := userServ.userUseCase.IsExist(in.USERNAME)
 	fmt.Println("isexist", in.USERNAME, err)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in isexist user").Inc()
+		metrics.User.WithLabelValues("500", "error in isexist user").Inc()
 		return &handler.NothingSec{}, err
 	}
-	metrics.Session.WithLabelValues("200", "success in isexist user").Inc()
+	metrics.User.WithLabelValues("200", "success in isexist user").Inc()
 	timer.ObserveDuration()
 	return &handler.NothingSec{Dummy: is}, nil
 }
@@ -161,14 +161,14 @@ func (userServ *UserServerImpl) GetUsersLike(ctx context.Context, in *handler.Us
 	}
 	users, err := userServ.userUseCase.GetUsersLike(in.USERNAME)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error in getuserslike user").Inc()
+		metrics.User.WithLabelValues("500", "error in getuserslike user").Inc()
 		return &handler.Users{}, err
 	}
 	bytesUsers, err := json.Marshal(users)
 	if err != nil {
-		metrics.Session.WithLabelValues("500", "error unmarshal in getuserslike user").Inc()
+		metrics.User.WithLabelValues("500", "error unmarshal in getuserslike user").Inc()
 	}
-	metrics.Session.WithLabelValues("200", "success in getuserslike user").Inc()
+	metrics.User.WithLabelValues("200", "success in getuserslike user").Inc()
 	timer.ObserveDuration()
 	return &handler.Users{USERS: bytesUsers}, nil
 }
