fb905d9a9823186edc4d1e5ea013e4d400ee72fb
yalublukotikov <yalublukotikov@yandex.ru>
2022-05-04 20:21:59 +0300

    новые тестики

---
+++ usecases/impl/comment_test.go
@@ -0,0 +1,36 @@
+package impl
+
+import (
+	customErrors "PLANEXA_backend/errors"
+	"PLANEXA_backend/models"
+	"PLANEXA_backend/repositories/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetComments(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	taskRepo := mock_repositories.NewMockTaskRepository(controller)
+	commentRepo := mock_repositories.NewMockCommentRepository(controller)
+	userRepo := mock_repositories.NewMockUserRepository(controller)
+	commentUseCase := MakeCommentUsecase(commentRepo, taskRepo, userRepo)
+
+	user := models.User{}
+	comments := []models.Comment{{Text: "title1"}}
+	commentUser := []models.Comment{{Text: "title1"}}
+	commentRepo.EXPECT().GetComments(uint(22)).Return(&comments, nil)
+	taskRepo.EXPECT().IsAccessToTask(uint(11), uint(0)).Return(true, nil)
+	userRepo.EXPECT().GetUserById(uint(0)).Return(&user, nil)
+	newComments, err := commentUseCase.GetComments(uint(11), uint(22))
+	assert.Equal(t, &commentUser, newComments)
+	assert.Equal(t, err, nil)
+
+	commentRepo.EXPECT().GetComments(uint(22)).Return(&comments, nil)
+	taskRepo.EXPECT().IsAccessToTask(uint(11), uint(0)).Return(false, customErrors.ErrBadInputData)
+	newComments, err = commentUseCase.GetComments(uint(11), uint(22))
+	assert.Equal(t, (*[]models.Comment)(nil), newComments)
+	assert.Equal(t, err, customErrors.ErrBadInputData)
+}
