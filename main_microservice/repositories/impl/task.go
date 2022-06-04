eafa6fb901728d4b60fe061bac7cec85a968b2e0
yalublukotikov <yalublukotikov@yandex.ru>
2022-06-04 13:13:14 +0300

    вот

--- main_microservice/repositories/impl/task.go
+++ main_microservice/repositories/impl/task.go
@@ -229,7 +229,7 @@ func (taskRepository *TaskRepositoryImpl) GetImportantTasks(IdU uint) (*[]models
 			*importantTasks = append(*importantTasks, (*tasks)[i])
 		}
 	}
-	return tasks, nil
+	return importantTasks, nil
 }
 
 func (taskRepository *TaskRepositoryImpl) GetTaskUser(IdT uint) (*[]models.User, error) {
